826e83833410fe84724947e9ae33f4bf540311a6
Nisal Bandara <thatndb@gmail.com>
2025-11-06 23:05:34 +0530

    lesson: hello-world v2
    with a lot of if statements

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("say hello to people", func(t *testing.T) {
-		got := Hello("Nisal")
+		got := Hello("Nisal", "")
 		want := "Hello, Nisal"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Renee", "French")
+		want := "Bonjour, Renee"
 		assertCorrectMessage(t, got, want)
 	})
 }
