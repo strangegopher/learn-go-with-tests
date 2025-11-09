572f554b94710f8eb762cf0fe24b8b502a3dca0e
Nisal Bandara <thatndb@gmail.com>
2025-11-09 20:03:49 +0530

    lesson: iteration v5
    refactor Repeat function to accept a parameter of the repeat count
    
    add more tests

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,9 +3,27 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("when a non-empty string is repeated", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertRepeatResult(t, expected, repeated)
+	})
 
+	t.Run("when an empty string is repeated", func(t *testing.T) {
+		repeated := Repeat("", 5)
+		expected := ""
+		assertRepeatResult(t, expected, repeated)
+	})
+
+	t.Run("when repeat count is zero or negative", func(t *testing.T) {
+		repeated := Repeat("a", -5)
+		expected := ""
+		assertRepeatResult(t, expected, repeated)
+	})
+}
+
+func assertRepeatResult(t testing.TB, expected, repeated string) {
+	t.Helper()
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
@@ -13,6 +31,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for b.Loop() {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
